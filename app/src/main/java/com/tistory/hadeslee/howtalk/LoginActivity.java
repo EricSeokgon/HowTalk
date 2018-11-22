39e1f8425f563b12eb4b37a36711eb3bf62e1c98
hadesyi <leesk55@gmail.com>
2018-11-23 01:56:55 +0900

    로그인 인터페이스 리스너

--- app/src/main/java/com/tistory/hadeslee/howtalk/LoginActivity.java
+++ app/src/main/java/com/tistory/hadeslee/howtalk/LoginActivity.java
@@ -3,33 +3,58 @@ package com.tistory.hadeslee.howtalk;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText id;
+    private EditText password;
+
     private Button login;
     private Button signup;
-    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    private FirebaseRemoteConfig firebaseRemoteConfig;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
-        String splash_background = mFirebaseRemoteConfig.getString(getString(R.string.rc_color));
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        String splash_background = firebaseRemoteConfig.getString(getString(R.string.rc_color));
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.parseColor(splash_background));
         }
 
-        login = (Button)findViewById(R.id.loginActivity_button_login);
-        signup = (Button)findViewById(R.id.loginActivity_button_signup);
+        id = (EditText) findViewById(R.id.loginActivity_edittext_id);
+        password = (EditText) findViewById(R.id.loginActivity_edittext_password);
+
+        login = (Button) findViewById(R.id.loginActivity_button_login);
+        signup = (Button) findViewById(R.id.loginActivity_button_signup);
         login.setBackgroundColor(Color.parseColor(splash_background));
         signup.setBackgroundColor(Color.parseColor(splash_background));
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginEvent();
+            }
+        });
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,5 +62,32 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    void loginEvent() {
+        firebaseAuth.signInWithEmailAndPassword(id.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            //로그인 실패한부분
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        //로그인 인터페이스 리스너
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //로그인
+                } else {
+                    //로그아웃
+                }
+
+            }
+        };
     }
 }
